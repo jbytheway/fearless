e4b4952401d69709bd9feb6bb379be9418c1bb09
John Bytheway <jbytheway@gmail.com>
2010-01-24 16:18:28 +0000

    Moved System into fearless::physics
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

---
+++ fearless/physics/system.hpp
@@ -0,0 +1,12 @@
+#ifndef FEARLESS_PHYSICS__SYSTEM_HPP
+#define FEARLESS_PHYSICS__SYSTEM_HPP
+
+namespace fearless { namespace physics {
+
+class System {
+};
+
+}}
+
+#endif // FEARLESS_PHYSICS__SYSTEM_HPP
+
