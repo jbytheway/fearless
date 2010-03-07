a6d784bd8ae501669d7eec344b0b68f250b613a5
John Bytheway <jbytheway@gmail.com>
2010-03-07 20:22:16 +0000

    Read stars.txt at start of explorer
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- bin/explore/main.cpp
+++ bin/explore/main.cpp
@@ -1,3 +1,7 @@
+#include <boost/filesystem/fstream.hpp>
+
+#include <fearless/physics/stardb.hpp>
+
 #include "glutmain.hpp"
 #include "renderer.hpp"
 
@@ -5,10 +9,15 @@ int main(int argc, char** argv)
 {
   boost::filesystem::path exe(argv[0]);
   boost::filesystem::path exePath = exe.parent_path();
+  boost::filesystem::path dataPath = exePath/".."/".."/".."/"data";
+
+  fearless::physics::StarDb starDb;
+  boost::filesystem::ifstream starsTxt(dataPath/"celestia_stars.txt");
+  starDb.load_celestia_txt(starsTxt);
 
   fearless::explore::GlutMain m(&argc, argv);
   /** \todo Worry a bit more about where to search */
-  fearless::explore::TextureSource texSrc(exePath/".."/".."/".."/"data");
+  fearless::explore::TextureSource texSrc(dataPath);
   fearless::explore::Renderer r(texSrc);
   m.go(r);
 }
