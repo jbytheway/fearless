ed73412601adcfa8d46fc7294c5435272b56f7c0
John Bytheway <jbytheway@gmail.com>
2010-03-14 21:41:36 +0000

    Weakened standard_tolerance to a more float-y value
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- fearless/maths/standard_tolerance.hpp
+++ fearless/maths/standard_tolerance.hpp
@@ -3,7 +3,12 @@
 
 namespace fearless { namespace maths {
 
-static double const standard_tolerance = 1e-15;
+/** \brief Tolerance used by default in very_near_to.
+ *
+ * 1e-8 is reasonable if floats were ever involved; 1e-15 is perhaps better for
+ * doubles.
+ */
+static double const standard_tolerance = 1e-8;
 
 }}
 
