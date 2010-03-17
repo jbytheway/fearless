71bda48fd3efeb16aa9b5be2c3b85929a785b713
John Bytheway <jbytheway@gmail.com>
2010-03-17 20:37:19 +0000

    Added units::acceleration and physics::Acceleration
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

---
+++ fearless/physics/acceleration.hpp
@@ -0,0 +1,26 @@
+#ifndef FEARLESS_PHYSICS__ACCELERATION_HPP
+#define FEARLESS_PHYSICS__ACCELERATION_HPP
+
+#include <fearless/units/acceleration.hpp>
+#include <fearless/physics/fourvector.hpp>
+
+namespace fearless { namespace physics {
+
+template<typename T>
+class Acceleration :
+  public ThreeVector<units::quantity<units::acceleration, T>> {
+  public:
+    typedef ThreeVector<units::quantity<units::acceleration, T>> base_type;
+    typedef typename base_type::quantity quantity;
+
+    Acceleration(base_type const& v) : base_type(v) {}
+    Acceleration(quantity x, quantity y, quantity z) :
+      base_type(std::move(x), std::move(y), std::move(z))
+    {}
+};
+
+
+}}
+
+#endif // FEARLESS_PHYSICS__ACCELERATION_HPP
+
---
+++ fearless/units/acceleration.hpp
@@ -0,0 +1,15 @@
+#ifndef FEARLESS_UNITS__ACCELERATION_HPP
+#define FEARLESS_UNITS__ACCELERATION_HPP
+
+#include <boost/units/systems/si/acceleration.hpp>
+
+namespace fearless { namespace units {
+
+using boost::units::si::acceleration;
+using boost::units::si::metres_per_second_squared;
+using boost::units::si::metre_per_second_squared;
+
+}}
+
+#endif // FEARLESS_UNITS__ACCELERATION_HPP
+
