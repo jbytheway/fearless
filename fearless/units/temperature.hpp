7ef211a49c7d8cb9067a4b2446bbb50452f178a9
John Bytheway <jbytheway@gmail.com>
2010-03-21 19:11:20 +0000

    Added units::temperature
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

---
+++ fearless/units/temperature.hpp
@@ -0,0 +1,14 @@
+#ifndef FEARLESS_UNITS__TEMPERATURE_HPP
+#define FEARLESS_UNITS__TEMPERATURE_HPP
+
+#include <boost/units/systems/si/temperature.hpp>
+
+namespace fearless { namespace units {
+
+using boost::units::si::temperature;
+using boost::units::si::kelvin;
+
+}}
+
+#endif // FEARLESS_UNITS__TEMPERATURE_HPP
+
