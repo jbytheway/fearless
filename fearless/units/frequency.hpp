0954a58a13215c73f56cc1f53da9849f22f47936
John Bytheway <jbytheway@gmail.com>
2010-03-27 21:59:35 +0000

    units/frequency
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

---
+++ fearless/units/frequency.hpp
@@ -0,0 +1,14 @@
+#ifndef FEARLESS_UNITS__FREQUENCY_HPP
+#define FEARLESS_UNITS__FREQUENCY_HPP
+
+#include <boost/units/systems/si/frequency.hpp>
+
+namespace fearless { namespace units {
+
+using boost::units::si::frequency;
+using boost::units::si::hertz;
+
+}}
+
+#endif // FEARLESS_UNITS__FREQUENCY_HPP
+
