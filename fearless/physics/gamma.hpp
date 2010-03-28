8b3768e2b583be4910fe9a259cb3d09e6b84eead
John Bytheway <jbytheway@gmail.com>
2010-03-28 13:08:10 +0100

    Missing includes and asserts in physics/gamma
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- fearless/physics/gamma.hpp
+++ fearless/physics/gamma.hpp
@@ -1,12 +1,16 @@
 #ifndef FEARLESS_PHYSICS__GAMMA_HPP
 #define FEARLESS_PHYSICS__GAMMA_HPP
 
+#include <fearless/units/quantity.hpp>
+#include <fearless/units/velocity.hpp>
+
 namespace fearless { namespace physics {
 
 template<typename Reality, typename T>
 T gamma(units::quantity<units::velocity, T> const v) {
   auto const v_squared = v*v;
   auto const c_squared = Reality::c.quantity()*Reality::c.quantity();
+  assert(v_squared < c_squared);
   return 1.0/sqrt(1.0-v_squared/c_squared);
 }
 
@@ -14,6 +18,7 @@ template<typename Reality, typename Velocity>
 typename Velocity::value_type gamma(Velocity const& v) {
   auto const v_norm_squared = v.norm_squared();
   auto const c_squared = Reality::c.quantity()*Reality::c.quantity();
+  assert(v_norm_squared < c_squared);
   return 1.0/sqrt(1.0-v_norm_squared/c_squared);
 }
 
